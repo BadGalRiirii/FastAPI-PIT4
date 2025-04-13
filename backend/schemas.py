2f5af6a887cda01479f3c289ba518c6f1f9191b4
BadGalRiirii <nicolasruilan30@gmail.com>
2025-04-14 04:06:29 +0800

    Add backend folder

---
+++ backend/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class TodoBase(BaseModel):
+    title: str
+    completed: bool = False
+
+class TodoCreate(TodoBase):
+    pass
+
+class TodoUpdate(TodoBase):
+    pass
+
+class TodoOut(TodoBase):
+    id: int
+
+    model_config = {
+        "from_attributes": True
+    }
